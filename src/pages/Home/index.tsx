5690d342d1d11edf7b3401fbe430f50f5073dac1
Jefferson00 <jeffersonts00@gmail.com>
2022-03-10 20:22:43 -0300

    feat: home layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,26 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { ScrollView, Text, TouchableOpacity, Dimensions } from 'react-native';
 import api from '../../services/api';
 import { useAuth } from '../../hooks/AuthContext';
 import * as S from './styles';
 import Header from '../../components/Header';
+import { Colors } from '../../styles/global';
+import Menu from '../../components/Menu';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default function Home() {
   const { user, signOut } = useAuth();
   const [users, setUsers] = useState<any[]>([]);
   const controller = new AbortController();
+  const width = Dimensions.get('screen').width;
+  const primaryColor = Colors.BLUE_PRIMARY_LIGHTER;
+  const secondaryColor = Colors.BLUE_SOFT_LIGHTER;
+
+  const estimateColors = {
+    month: Colors.MAIN_TEXT_LIGHTER,
+    value: Colors.BLUE_SECONDARY_LIGHTER,
+    indicator: Colors.ORANGE_SECONDARY_LIGHTER,
+  };
 
   const getApiUsersExample = useCallback(async () => {
     try {
@@ -27,13 +39,158 @@ export default function Home() {
     };
   }, []);
 
-  return (
-    <S.Container>
-      <Header />
+  const cards = [
+    {
+      id: 1,
+      title: 'Banco do Brasil',
+      current_balance: 100000,
+      estimate_balance: 80000,
+    },
+    {
+      id: 2,
+      title: 'Nubank',
+      current_balance: 100000,
+      estimate_balance: 80000,
+    },
+  ];
 
-      <TouchableOpacity onPress={() => signOut()}>
-        <Text>SAIR</Text>
-      </TouchableOpacity>
-    </S.Container>
+  const estimates = [
+    {
+      id: 1,
+      month: 'Nov 20',
+      value: 100000,
+      indicator: 0,
+    },
+    {
+      id: 2,
+      month: 'Dez 20',
+      value: 1000000,
+      indicator: 20,
+    },
+    {
+      id: 3,
+      month: 'Jan 21',
+      value: 1000000,
+      indicator: 20,
+    },
+    {
+      id: 4,
+      month: 'Fev 21',
+      value: 1000000,
+      indicator: 20,
+    },
+    {
+      id: 5,
+      month: 'Mar 21',
+      value: 2000000,
+      indicator: 40,
+    },
+  ];
+
+  return (
+    <>
+      <ScrollView
+        style={{
+          flex: 1,
+        }}>
+        <Header />
+        <S.Container>
+          <ScrollView
+            horizontal
+            snapToInterval={width}
+            decelerationRate="fast"
+            showsHorizontalScrollIndicator={false}
+            style={{
+              height: 149,
+            }}>
+            {cards.map(card => (
+              <S.AccountCardWrapper key={card.id}>
+                <S.Card backgroundColor="#FF981E">
+                  <S.CardInfo>
+                    <S.CardTitle>{card.title}</S.CardTitle>
+                    <S.CardBalance>R$ {card.current_balance}</S.CardBalance>
+                    <S.CardSubBalance>
+                      Previsto R$ {card.estimate_balance}
+                    </S.CardSubBalance>
+                  </S.CardInfo>
+
+                  <S.IconContainer>
+                    <Icon name="business" size={32} color="#FF981E" />
+                  </S.IconContainer>
+                </S.Card>
+              </S.AccountCardWrapper>
+            ))}
+            <S.AccountCardWrapper>
+              <S.Card backgroundColor="#FF981E">
+                <Text>add</Text>
+              </S.Card>
+            </S.AccountCardWrapper>
+          </ScrollView>
+
+          <S.AccountDots>
+            {cards.map(card => (
+              <S.Dot key={card.id} active={card.id === 1} />
+            ))}
+          </S.AccountDots>
+
+          <S.BalanceContainer>
+            <S.Balance>
+              <S.BalanceText color={primaryColor}>Saldo atual</S.BalanceText>
+              <S.BalanceValue>R$ 0</S.BalanceValue>
+            </S.Balance>
+
+            <S.Balance>
+              <S.BalanceText color={primaryColor}>Saldo previsto</S.BalanceText>
+              <S.BalanceValue>R$ 0</S.BalanceValue>
+            </S.Balance>
+          </S.BalanceContainer>
+
+          <S.Estimates>
+            <S.BalanceText color={primaryColor}>Estimativas</S.BalanceText>
+            <S.EstimateView backgroundColor={secondaryColor}>
+              <ScrollView
+                horizontal
+                contentContainerStyle={{
+                  minWidth: '100%',
+                  alignItems: 'flex-end',
+                  justifyContent: 'center',
+                }}
+                showsHorizontalScrollIndicator={false}>
+                {estimates.map(estimate => (
+                  <S.EstimateInMonth key={estimate.id}>
+                    <S.EstimateMonthText monthTextColor={estimateColors.month}>
+                      {estimate.month}
+                    </S.EstimateMonthText>
+                    <S.EstimateMonthValue valueTextColor={estimateColors.value}>
+                      R$ {estimate.value}
+                    </S.EstimateMonthValue>
+
+                    <S.EstimateIndicator
+                      indicatorVelue={estimate.indicator}
+                      indicatorColor={estimateColors.indicator}
+                    />
+                  </S.EstimateInMonth>
+                ))}
+              </ScrollView>
+            </S.EstimateView>
+          </S.Estimates>
+
+          <S.LastTransactions>
+            <S.BalanceText color={primaryColor}>
+              Últimas Transações
+            </S.BalanceText>
+
+            <S.LastTransactionsView
+              backgroundColor={secondaryColor}></S.LastTransactionsView>
+            <S.LastTransactionsView
+              backgroundColor={secondaryColor}></S.LastTransactionsView>
+          </S.LastTransactions>
+          {/* <TouchableOpacity onPress={() => signOut()}>
+          <Text>SAIR</Text>
+        </TouchableOpacity> */}
+        </S.Container>
+      </ScrollView>
+      <Menu />
+    </>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,7 +1,202 @@
 import React from 'react';
-import styled from "styled-components/native";
+import { Dimensions } from 'react-native';
+import { RFPercentage } from 'react-native-responsive-fontsize';
+import styled, { css } from 'styled-components/native';
+const width = Dimensions.get('screen').width;
+
+interface CardProps {
+  backgroundColor?: string;
+}
+
+interface BalanceProps {
+  color: string;
+}
+interface EstimatesProps {
+  backgroundColor?: string;
+  monthTextColor?: string;
+  valueTextColor?: string;
+  indicatorColor?: string;
+  indicatorVelue?: number;
+}
+
+interface LastTransactionsProps {
+  backgroundColor?: string;
+}
+
+interface DotProps {
+  active?: boolean;
+}
 
 export const Container = styled.View`
-  flex: 1;
+  justify-content: flex-start;
+  background-color: transparent;
+  position: relative;
+  top: -5%;
+
+  padding-bottom: 86px;
+`;
+
+export const AccountCardWrapper = styled.View`
+  width: ${width}px;
+  height: 149px;
+`;
+
+export const Card = styled.View<CardProps>`
+  width: 269px;
+  height: 149px;
+  margin: 0 auto;
+  border-radius: 10px;
+
+  padding: 24px;
+
+  flex-direction: row;
+`;
+
+export const CardInfo = styled.View<CardProps>`
+  width: 80%;
+  height: 100%;
+  justify-content: flex-start;
+`;
+
+export const CardTitle = styled.Text<CardProps>`
+  font-family: 'Poppins-SemiBold';
+  font-size: ${RFPercentage(2.5)}px;
+
+  color: #fff;
+`;
+
+export const CardBalance = styled.Text<CardProps>`
+  font-family: 'Poppins-SemiBold';
+  font-size: ${RFPercentage(3)}px;
+
+  margin-top: 16px;
+  color: #fff;
+`;
+
+export const CardSubBalance = styled.Text<CardProps>`
+  font-family: 'Poppins-SemiBold';
+  font-size: ${RFPercentage(1.8)}px;
+
+  line-height: 16px;
+  opacity: 0.8;
+  color: #fff;
+`;
+
+export const IconContainer = styled.View`
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+
+  align-items: center;
+  justify-content: center;
+
   background-color: #fff;
+`;
+
+export const AccountDots = styled.View`
+  flex-direction: row;
+  justify-content: center;
+
+  padding: 16px;
+`;
+
+export const Dot = styled.View<DotProps>`
+  width: 15px;
+  height: 15px;
+
+  border-radius: 6.5px;
+
+  background-color: #f9c33c;
+
+  margin: 0 8px;
+  opacity: 0.6;
+
+  ${props =>
+    props.active &&
+    css`
+      border: 1px solid #ff981e;
+      opacity: 1;
+    `}
+`;
+
+export const BalanceContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+
+  padding: 0 16px;
+  margin-top: 32px;
+`;
+export const Balance = styled.View`
+  justify-content: center;
+  align-items: center;
+`;
+export const BalanceText = styled.Text<BalanceProps>`
+  font-family: 'Poppins-SemiBold';
+  font-size: ${RFPercentage(2.5)}px;
+
+  color: ${props => props.color};
+`;
+export const BalanceValue = styled.Text`
+  font-family: 'Poppins-SemiBold';
+  font-size: ${RFPercentage(2)}px;
+
+  margin-top: 16px;
+`;
+
+export const Estimates = styled.View`
+  padding: 0 16px;
+  margin-top: 16px;
+`;
+
+export const EstimateView = styled.View<EstimatesProps>`
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: flex-end;
+
+  height: 120px;
+  border-radius: 10px;
+  margin-top: 8px;
+  padding: 16px;
+  background-color: ${props => props.backgroundColor};
+`;
+
+export const EstimateInMonth = styled.View`
+  align-items: center;
+  max-height: 100%;
+  margin-right: 16px;
+`;
+
+export const EstimateMonthText = styled.Text<EstimatesProps>`
+  font-family: 'Poppins-SemiBold';
+  font-size: ${RFPercentage(1.8)}px;
+
+  color: ${props => props.monthTextColor};
+`;
+export const EstimateMonthValue = styled.Text<EstimatesProps>`
+  font-family: 'Poppins-SemiBold';
+  font-size: ${RFPercentage(1.5)}px;
+
+  color: ${props => props.valueTextColor};
+`;
+
+export const EstimateIndicator = styled.View<EstimatesProps>`
+  height: ${props =>
+    props.indicatorVelue ? `${props.indicatorVelue}px` : `4px`};
+  width: 100%;
+  max-height: 40px;
+  border-radius: 10px;
+  background-color: ${props => props.indicatorColor};
+`;
+
+export const LastTransactions = styled.View`
+  padding: 0 16px;
+  margin-top: 16px;
+`;
+
+export const LastTransactionsView = styled.View<LastTransactionsProps>`
+  min-height: 70px;
+  background-color: ${props => props.backgroundColor};
+
+  border-radius: 10px;
+  margin-top: 8px;
 `;
