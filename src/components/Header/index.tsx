d71932f4e51dcb1f4ee0561b08209f14527a017f
Jefferson00 <jeffersonts00@gmail.com>
2022-03-24 20:04:25 -0300

    feat: month selector animation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,27 +4,64 @@ import { Colors } from '../../styles/global';
 import Icons from 'react-native-vector-icons/Ionicons';
 import * as S from './styles';
 import { getMounthAndYear } from '../../utils/dateFormats';
+import {
+  useSharedValue,
+  useAnimatedStyle,
+  interpolate,
+  withTiming,
+  withSpring,
+  withSequence,
+} from 'react-native-reanimated';
 
 export default function Header() {
   const { user } = useAuth();
   const backgroundColor = Colors.BLUE_PRIMARY_LIGHTER;
+
+  const textAnimate = useSharedValue(0);
+  const textOpacity = useSharedValue(0);
+
+  const textAnimated = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: withSpring(textAnimate.value),
+        },
+      ],
+      opacity: interpolate(textOpacity.value, [0, 1], [1, 0]),
+    };
+  });
 
   const [selectorColor, setSelectorColor] = useState(
     Colors.ORANGE_PRIMARY_LIGHTER,
   );
   const [currentDate, setCurrentDate] = useState(new Date());
   const [selectedDate, setSelectedDate] = useState(currentDate);
+  const [disableButton, setDisableButton] = useState(false);
 
   const handleChangeMonth = useCallback(
-    (order: 'PREV' | 'NEXT') => {
-      const currentMonth = selectedDate.getMonth();
-      setSelectedDate(
-        new Date(
-          selectedDate.setMonth(
-            order === 'NEXT' ? currentMonth + 1 : currentMonth - 1,
-          ),
-        ),
+    async (order: 'PREV' | 'NEXT') => {
+      setDisableButton(true);
+      textAnimate.value = withSequence(
+        withTiming(order === 'NEXT' ? -300 : 100, { duration: 50 }),
+        withTiming(order === 'NEXT' ? 100 : -300),
+        withTiming(0, { duration: 50 }),
       );
+      textOpacity.value = withSequence(
+        withTiming(1),
+        withTiming(1),
+        withTiming(0, { duration: 50 }),
+      );
+      const currentMonth = selectedDate.getMonth();
+      setTimeout(() => {
+        setSelectedDate(
+          new Date(
+            selectedDate.setMonth(
+              order === 'NEXT' ? currentMonth + 1 : currentMonth - 1,
+            ),
+          ),
+        );
+        setDisableButton(false);
+      }, 500);
     },
     [selectedDate],
   );
@@ -43,13 +80,19 @@ export default function Header() {
   return (
     <S.Container backgroundColor={backgroundColor}>
       <S.MonthSelector>
-        <S.PrevButton onPress={() => handleChangeMonth('PREV')}>
+        <S.PrevButton
+          onPress={() => handleChangeMonth('PREV')}
+          disabled={disableButton}>
           <Icons name="chevron-back" size={32} color={selectorColor} />
         </S.PrevButton>
-        <S.Month color={selectorColor}>
-          {getMounthAndYear(selectedDate)}
-        </S.Month>
-        <S.NextButton onPress={() => handleChangeMonth('NEXT')}>
+        <S.MonthContainer>
+          <S.Month color={selectorColor} style={textAnimated}>
+            {getMounthAndYear(selectedDate)}
+          </S.Month>
+        </S.MonthContainer>
+        <S.NextButton
+          onPress={() => handleChangeMonth('NEXT')}
+          disabled={disableButton}>
           <Icons name="chevron-forward" size={32} color={selectorColor} />
         </S.NextButton>
       </S.MonthSelector>
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,45 +1,48 @@
 import styled from 'styled-components/native';
 import { RFPercentage } from 'react-native-responsive-fontsize';
+import Animated from 'react-native-reanimated';
 
 interface ContainerProps {
-  backgroundColor: string,
+  backgroundColor: string;
 }
 
 interface MonthProps {
-  color: string,
+  color: string;
 }
 
 export const Container = styled.View<ContainerProps>`
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  padding: 24px 24px 74px 16px;
-`
+  padding: ${RFPercentage(6)}px ${RFPercentage(3.2)}px ${RFPercentage(9.5)}px
+    ${RFPercentage(2.5)}px;
+`;
 export const Avatar = styled.Image`
-  height: 50px;
-  width: 50px;
-  border-radius: 25px;
-`
+  height: ${RFPercentage(6.5)}px;
+  width: ${RFPercentage(6.5)}px;
+  border-radius: ${RFPercentage(3.25)}px;
+`;
 export const EmptyAvatar = styled.View`
-  height: 50px;
-  width: 50px;
+  height: ${RFPercentage(6.5)}px;
+  width: ${RFPercentage(6.5)}px;
+  border-radius: ${RFPercentage(3.25)}px;
   background-color: #d2d2d2;
-  border-radius: 25px;
-`
+`;
 export const MonthSelector = styled.View`
   flex-direction: row;
   align-items: center;
-`
+`;
 
-export const PrevButton = styled.Pressable`
-
-`
-export const NextButton = styled.Pressable`
-
-`
-export const Month = styled.Text<MonthProps>`
-  color: ${(props) => props.color};
+export const PrevButton = styled.Pressable``;
+export const NextButton = styled.Pressable``;
+export const MonthContainer = styled.View`
+  width: ${RFPercentage(19)}px;
+  align-items: center;
+  overflow: hidden;
+`;
+export const Month = styled(Animated.Text)<MonthProps>`
+  color: ${props => props.color};
   font-family: 'Poppins-SemiBold';
   font-size: ${RFPercentage(3.2)}px;
-  margin: 0 ${RFPercentage(1)}px;;
-`
+  margin: 0 ${RFPercentage(1)}px; ;
+`;
